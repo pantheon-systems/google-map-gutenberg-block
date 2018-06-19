c7e38ab48aece7008aee4918af52a668efead630
Joe McGill <joemcgill@gmail.com>
2018-06-19 16:29:01 -0500

    Meet WP-VIP PHP code standards
    This fixes (or specifically ignores) PHPCS rules to pass WordPress-VIP coding standards.

--- embed-gutenberg-block-google-maps.php
+++ embed-gutenberg-block-google-maps.php
@@ -14,11 +14,11 @@
  * @package PantheonGoogleMapBlock
  */
 
- namespace PantheonGoogleMapBlock;
+namespace PantheonGoogleMapBlock;
 
 //  Exit if accessed directly.
-if (! defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
@@ -26,50 +26,49 @@ if (! defined('ABSPATH')) {
  *
  * @return void
  */
-function blockScripts()
-{
-    // Make paths variables so we don't write em twice ;)
-    $hash = '.8cebc8e7edf5e9177f43';
-    $blockPath = "assets/js/index$hash.js";
-    $stylePath = "assets/css/style$hash.css";
+function block_scripts() {
+	// Make paths variables so we don't write em twice ;)
+	$hash       = '.8cebc8e7edf5e9177f43';
+	$block_path = "assets/js/index$hash.js";
+	$style_path = "assets/css/style$hash.css";
 
-    // Register the bundled block JS file.
-    if (file_exists(plugin_dir_path(__FILE__) . $blockPath)) {
-        wp_register_script(
-            'pantheon-google-map-block-js',
-            plugins_url($blockPath, __FILE__),
-            array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ),
-            filemtime(plugin_dir_path(__FILE__) . $blockPath)
-        );
-    }
+	// Register the bundled block JS file.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $block_path ) ) {
+		wp_register_script(
+			'pantheon-google-map-block-js',
+			plugins_url( $block_path, __FILE__ ),
+			array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ),
+			filemtime( plugin_dir_path( __FILE__ ) . $block_path )
+		);
+	}
 
-    // Register frontend and editor block styles.
-    if (file_exists(plugin_dir_path(__FILE__) . $stylePath)) {
-        wp_register_style(
-            'pantheon-google-map-block-css',
-            plugins_url($stylePath, __FILE__),
-            array( 'wp-blocks' ),
-            filemtime(plugin_dir_path(__FILE__) . $stylePath)
-        );
-    }
+	// Register frontend and editor block styles.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $style_path ) ) {
+		wp_register_style(
+			'pantheon-google-map-block-css',
+			plugins_url( $style_path, __FILE__ ),
+			array( 'wp-blocks' ),
+			filemtime( plugin_dir_path( __FILE__ ) . $style_path )
+		);
+	}
 
 }
 
-function registerSettings() {
-    register_setting(
-        'pantheon_google_map_block_api_key',
-        'pantheon_google_map_block_api_key',
-        array(
-            'type' => 'string',
-            'description' => __('Google Map API key for the Gutenberg block plugin.'),
-            'sanitize_callback' => 'sanitize_text_field',
-            'show_in_rest' => true,
-            'default' => ''
-        )
-    );
+function register_settings() {
+	register_setting(
+		'pantheon_google_map_block_api_key',
+		'pantheon_google_map_block_api_key',
+		array(
+			'type'              => 'string',
+			'description'       => __( 'Google Map API key for the Gutenberg block plugin.' ),
+			'sanitize_callback' => 'sanitize_text_field',
+			'show_in_rest'      => true,
+			'default'           => '',
+		)
+	);
 }
 
-add_action( 'init', __NAMESPACE__ . '\\registerSettings'  );
+add_action( 'init', __NAMESPACE__ . '\\register_settings' );
 
 /**
  * Render the Google Map block.
@@ -77,58 +76,60 @@ add_action( 'init', __NAMESPACE__ . '\\registerSettings'  );
  * @param array $attributes An array of block attributes and values.
  * @return string The rendered HTML output of the block.
  */
-function renderGutenbergMapEmbedblock( $attributes ) {
+function render_gutenberg_map_embed_block( $attributes ) {
 
-    // Get the API key.
-    $APIkey = get_option('pantheon_google_map_block_api_key');
+	// Get the API key.
+	$api_key = get_option( 'pantheon_google_map_block_api_key' );
 
-    // Don't output anything if there is no API key.
-    if (null === $APIkey || empty( $APIkey )) {
-        return;
-    }
+	// Don't output anything if there is no API key.
+	if ( null === $api_key || empty( $api_key ) ) {
+		return;
+	}
 
-    // Expand all the attributes into separate variables.
-    foreach($attributes as $key=>$value) {
-        ${$key} = $value;
-    }
+	// Expand all the attributes into separate variables.
+	foreach ( $attributes as $key => $value ) {
+		${$key} = $value;
+	}
 
-    // URL encode the location for Google Maps.
-    $location = urlencode ( $location );
+	// URL encode the location for Google Maps.
+	$location = rawurlencode( $location );
 
-    // Set the API URL based to embed or static maps based on the interactive setting.
-    $apiURL = ( $interactive ) ? "https://www.google.com/maps/embed/v1/place?key=${APIkey}&q=${location}&zoom=${zoom}&maptype=${mapType}" : "https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=${zoom}&size=${maxWidth}x${maxHeight}&maptype=${mapType}&key=${APIkey}";
+	// Set the API URL based to embed or static maps based on the interactive setting.
+	$api_url = ( $interactive ) ? "https://www.google.com/maps/embed/v1/place?key=${api_key}&q=${location}&zoom=${zoom}&maptype=${mapType}" : "https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=${zoom}&size=${maxWidth}x${maxHeight}&maptype=${mapType}&key=${api_key}"; /* phpcs:ignore */
 
-    // Check status code of apiURL.
-    $ch = curl_init($apiURL);
-    curl_setopt($ch, CURLOPT_HEADER, true);
-    curl_setopt($ch, CURLOPT_NOBODY, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch, CURLOPT_TIMEOUT,10);
-    $output = curl_exec($ch);
-    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
+	// Check status code of the API.
+	// phpcs:disable WordPress.WP.AlternativeFunctions
+	$ch = curl_init( $api_url );
+	curl_setopt( $ch, CURLOPT_HEADER, true );
+	curl_setopt( $ch, CURLOPT_NOBODY, true );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
+	$output   = curl_exec( $ch );
+	$httpcode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+	curl_close( $ch );
+	// phpcs:enable WordPress.WP.AlternativeFunctions
 
-    // Don't output anything if the response from Google Maps isn't a 200.
-    if( $httpcode !== 200 ){
-        return;
-    }
+	// Don't output anything if the response from Google Maps isn't a 200.
+	if ( 200 !== $httpcode ) {
+		return;
+	}
 
-    // Set the appropriate CSS class names.
-    $classNames = ( $interactive ) ? "wp-block-pantheon-google-map interactive ratio$aspectRatio" : "wp-block-pantheon-google-map";
+	// Set the appropriate CSS class names.
+	$classes = ( $interactive ) ? "wp-block-pantheon-google-map interactive ratio$aspectRatio" : 'wp-block-pantheon-google-map'; /* phpcs:ignore */
 
-    // Create the output.
-    $output = "<div class='" . esc_attr( $classNames ) . "'><div class='map'>";
-    // If the map is interactive show the iframe.
-    if ( $interactive ){
-        $output .= "<iframe width='100%' height='100%' frameborder='0' style='border:0' src='" . esc_url( $apiURL ) . "' allowfullscreen></iframe>";
-    // Otherwise use the static API.
-    } else {
-        $output .= "<img src='" . esc_url( $apiURL ) . "' />";
-    }
-    $output .= '</div></div>';
+	// Create the output.
+	$output = "<div class='$classes'><div class='map'>";
+	// If the map is interactive show the iframe.
+	if ( $interactive ) {
+		$output .= "<iframe width='100%' height='100%' frameborder='0' style='border:0' src='$api_url' allowfullscreen></iframe>";
+		// Otherwise use the static API.
+	} else {
+		$output .= "<img src='$api_url' />";
+	}
+	$output .= '</div></div>';
 
-    // Return the output.
-    return $output;
+	// Return the output.
+	return $output;
 }
 
 /**
@@ -136,46 +137,48 @@ function renderGutenbergMapEmbedblock( $attributes ) {
  *
  * @return void
  */
-function registerMapBlock() {
-    if( \function_exists('register_block_type') ){
-        blockScripts();
-        \register_block_type( 'pantheon/google-map', array(
-            'editor_script' => 'pantheon-google-map-block-js',
-            'style' => 'pantheon-google-map-block-css',
-            'attributes' => array (
-                'location' => array (
-                    'type' => 'string',
-                    'default' => '',
-                ),
-                'mapType' => array (
-                    'type' => 'string',
-                    'default' => 'roadmap',
-                ),
-                'zoom' => array (
-                    'type' => 'number',
-                    'default' => 13,
-                ),
-                'maxWidth' => array (
-                    'type' => 'number',
-                    'default' => 1920,
-                ),
-                'maxHeight' => array (
-                    'type' => 'number',
-                    'default' => 1329,
-                ),
-                'interactive' => array (
-                    'type' => 'boolean',
-                    'default' => true,
-                ),
-                'aspectRatio' => array (
-                    'type' => 'string',
-                    'default' => '2_1',
-                ),
-            ),
-            'render_callback' => __NAMESPACE__ . '\\renderGutenbergMapEmbedblock',
-        ) );
-    }
+function register_map_block() {
+	if ( \function_exists( 'register_block_type' ) ) {
+		block_scripts();
+		\register_block_type(
+			'pantheon/google-map', array(
+				'editor_script'   => 'pantheon-google-map-block-js',
+				'style'           => 'pantheon-google-map-block-css',
+				'attributes'      => array(
+					'location'    => array(
+						'type'    => 'string',
+						'default' => '',
+					),
+					'mapType'     => array(
+						'type'    => 'string',
+						'default' => 'roadmap',
+					),
+					'zoom'        => array(
+						'type'    => 'number',
+						'default' => 13,
+					),
+					'maxWidth'    => array(
+						'type'    => 'number',
+						'default' => 1920,
+					),
+					'maxHeight'   => array(
+						'type'    => 'number',
+						'default' => 1329,
+					),
+					'interactive' => array(
+						'type'    => 'boolean',
+						'default' => true,
+					),
+					'aspectRatio' => array(
+						'type'    => 'string',
+						'default' => '2_1',
+					),
+				),
+				'render_callback' => __NAMESPACE__ . '\\render_gutenberg_map_embed_block',
+			)
+		);
+	}
 }
 
 
-add_action('init', __NAMESPACE__ . '\\registerMapBlock' );
+add_action( 'init', __NAMESPACE__ . '\\register_map_block' );
